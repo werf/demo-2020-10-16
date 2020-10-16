c332b0db23d37fa42f0b70f3ec68f3f4248eb904
Dmitry Stolyarov <dmitry.stolyarov@flant.com>
2020-10-16 15:56:27 +0000

    050-frontend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/api/', function (req, res) {
+  res.send('server rendered text');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
